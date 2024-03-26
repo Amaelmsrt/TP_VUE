238fda87e40170e91e2915ddc3c653869c8074ee
Amael Maserati <amael.maserati@etu.univ-orleans.fr>
2024-03-26 18:13:56 +0100

    ajout des composants pour les questions

---
+++ quiz/src/components/QuestionSimple.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="question">
+        <h2>{{ question.title }}</h2>
+        <p>{{ question.reponse }}</p>
+        <button class="btn btn-danger" @click="deleteQuestion">Supprimer</button>
+        <button class="btn btn-primary" @click="editQuestion">Modifier</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            question: Object,
+            required: true
+        },
+        methods: {
+            deleteQuestion() {
+                // Logique pour supprimer la question
+            },
+            editQuestion() {
+                // Logique pour modifier la question
+            }
+        }
+    }
+</script>
